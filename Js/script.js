65a9fa0406a56b06d8161133eb1b73bfcffa1579
Kujtim Krasniqi <36826448+Kujt@users.noreply.github.com>
2022-03-23 13:26:43 +0100

    creating Detail country info

--- Js/script.js
+++ Js/script.js
@@ -2,8 +2,12 @@ const dropBtn = document.querySelector(".dropbtn");
 const dropContent = document.querySelector(".dropdown-content");
 const regionF = dropContent.querySelectorAll("a");
 const countriesContainer = document.querySelector(".countries");
-const countryData = document.querySelector(".country");
+const modal = document.querySelector(".country-detail");
+const modalBody = document.querySelector(".modal-body");
 const input = document.querySelector("#search");
+const modalCloseBtn = document.getElementById("close");
+const search = document.querySelector(".search");
+const closeBtn = document.querySelector(".detail-btn");
 
 window.addEventListener("click", function (e) {
   if (dropBtn.contains(e.target)) {
@@ -40,8 +44,76 @@ function displayCountries(countries) {
             </div>
     `;
 
+    countryEL.addEventListener("click", function () {
+      closeBtn.style.display = "block";
+      countriesContainer.style.display = "none";
+      search.style.display = "none";
+      modal.style.display = "flex";
+      showCountryDetails(country);
+    });
+
     countriesContainer.appendChild(countryEL);
   });
+}
+
+function showCountryDetails(country) {
+  modalBody.innerHTML = `
+ 
+<div class="details">
+<div class="details__flag">
+  <img src="${country.flag}" alt="${name}" />
+</div>
+<div class="details__about">
+  <div class="details__about--title">
+    <h2>${country.name}</h2>
+  </div>
+  <div class="details__about--left">
+    <p>
+      Native name:
+      <span class="details_about--native-name">${country.nativeName}</span>
+    </p>
+    <p>
+      Population:
+      <span class="details__about--population">${country.population}</span>
+    </p>
+    <p>
+      Region:
+      <span class="details__about--region">${country.region}</span>
+    </p>
+    <p>
+      Sub Region:
+      <span class="details__about--sub-region"> ${country.subregion}</span>
+    </p>
+    <p>
+      Capital:
+      <span class="details__about--capital"> ${country.capital}</span>
+    </p>
+  </div>
+  <div class="details__about--right">
+    <p>
+      Top Level Domain:
+      <span class="details__about--top-domain"> ${
+        country.topLevelDomain[0]
+      }</span>
+    </p>
+    <p>
+      Currencies:
+      <span class="details__about--currencies">  ${country.currencies.map(
+        (currency) => currency.code
+      )}</span>
+    </p>
+    <p>
+      Languages:
+      <span class="details__about--languages">  ${country.languages.map(
+        (language) => language.name
+      )}</span>
+    </p>
+  </div>
+  
+</div>
+</div>
+
+`;
 }
 
 input.addEventListener("input", function (e) {
@@ -70,4 +142,10 @@ regionF.forEach((f) => {
       }
     });
   });
+});
+
+modalCloseBtn.addEventListener("click", function () {
+  modal.style.display = "none";
+  countriesContainer.style.display = "grid";
+  search.style.display = "flex";
 });
